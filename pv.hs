4180e26cc52d920b82800ad2e5a5a31bc5b71997
Luca <31829436+Ifs26@users.noreply.github.com>
2024-09-22 15:41:45 -0300

    Hice Parcial 1 (2024/04/22) Tema D (1:10hs aprox)

--- pv.hs
+++ pv.hs
@@ -7,7 +7,7 @@ data MaterialesConstruccion = Ladrillo TipoLadrillo UsoDeLadrillo Precio
                             | Vigueta Largo MaterialViga Precio
                             | Cemento MarcaCemento deriving(Show)
 
-type Precio = Int
+--type Precio = Int
 
 data TipoLadrillo = Ceramico | Hormigon | Tradicional deriving(Eq,Show)
 data UsoDeLadrillo = Pared | Techo deriving(Show)
@@ -79,3 +79,115 @@ devolverAltura (EvolucionDelArbol nm est alt diam aprg arbNat) nm_tc | nm == nm_
                                                                     | otherwise= devolverAltura arbNat nm_tc
 
 setarb = (EvolucionDelArbol "Alamo" EnFlor 8 8 9(EvolucionDelArbol "Pino" ConFrutos 4 5 6(EvolucionDelArbol "Nogal" Verde 10 10 10(EvolucionDelArbol "Seibo" EnFlor 2 2 2 NoHayMasArboles))))                                         
+
+
+---------------------------------------------------------------------------------------
+
+type Nombre = String
+data Genero = Drama | Romance | Misterio | Aventura deriving(Show)
+type Autor = String
+type NumP = Int
+type Precio = Int
+
+data Tema = Politica | Filosofia | Ciencia | Historia deriving(Show)
+
+data Estilo = Clasica | Contemporanea | Experimental deriving(Show)
+
+data AreaDE = Astronomia | Fisica | Matematica | Computacion deriving(Show)
+
+data Libro = Novela Nombre Genero Autor NumP Precio 
+            | Ensayo Nombre Tema Autor NumP 
+            | Poesia Nombre Estilo Autor NumP Precio 
+            | Cientifico Nombre AreaDE Autor NumP
+            deriving(Show)
+
+cuantasNovelasPorGenero :: [Libro] -> Genero -> Int
+cuantasNovelasPorGenero [] gen_tc = 0
+cuantasNovelasPorGenero ((Novela nom Drama au num pre):xs) Drama = 1 + cuantasNovelasPorGenero xs Drama 
+cuantasNovelasPorGenero ((Novela nom Romance au num pre):xs) Romance = 1 + cuantasNovelasPorGenero xs Romance
+cuantasNovelasPorGenero ((Novela nom Misterio au num pre):xs) Misterio = 1 + cuantasNovelasPorGenero xs Misterio
+cuantasNovelasPorGenero ((Novela nom Aventura au num pre):xs) Aventura = 1 + cuantasNovelasPorGenero xs Aventura 
+cuantasNovelasPorGenero ((Novela nom _ au num pre):xs) gen_tc = cuantasNovelasPorGenero xs gen_tc
+cuantasNovelasPorGenero (_:xs) gen_tc = cuantasNovelasPorGenero xs gen_tc
+
+li1 = (Novela "asd" Drama "Juancho" 125 2500)
+li2 = (Novela "asd" Romance "Juancho" 250 1000)
+li3 = Ensayo "adasd" Politica "Benito" 500
+
+-- cuantasNovelasPorGenero [li3,li2,li1] Drama
+-- 1
+
+mismoGenero :: Genero -> Genero -> Bool
+mismoGenero Drama Drama = True
+mismoGenero Romance Romance = True
+mismoGenero Misterio Misterio = True
+mismoGenero Aventura Aventura = True
+mismoGenero _ _ = False
+
+-- Politica | Filosofia | Ciencia | Historia
+mismoTema :: Tema -> Tema -> Bool
+mismoTema Politica Politica = True
+mismoTema Filosofia Filosofia = True
+mismoTema Ciencia Ciencia = True
+mismoTema Historia Historia = True
+mismoTema _ _ = False
+
+-- Clasica | Contemporanea | Experimental
+mismoEstilo :: Estilo -> Estilo -> Bool
+mismoEstilo Clasica Clasica = True
+mismoEstilo Contemporanea Contemporanea = True
+mismoEstilo Experimental Experimental = True
+mismoEstilo _ _ = False
+
+-- Astronomia | Fisica | Matematica | Computacion
+mismoADE :: AreaDE -> AreaDE -> Bool
+mismoADE Astronomia Astronomia = True
+mismoADE Fisica Fisica = True
+mismoADE Matematica Matematica = True 
+mismoADE Computacion Computacion = True
+mismoADE _ _ = False
+
+instance Eq Libro 
+    where 
+        (Novela n1 g1 a1 np1 p1) == (Novela n2 g2 a2 np2 p2) = (a1 == a2) && (mismoGenero g1 g2) && (n1 == n2)
+        (Ensayo n1 t1 a1 np1) == (Ensayo n2 t2 a2 np2) = (n1 == n2) && (mismoTema t1 t2) && (a1 == a2)
+        (Poesia n1 e1 a1 np1 p1) == (Poesia n2 e2 a2 np2 p2) = (n1 == n2) && (mismoEstilo e1 e2) && (a1 == a2)
+        (Cientifico n1 ade1 a1 np1) == (Cientifico n2 ade2 a2 np2) = (n1 == n2) && (mismoADE ade1 ade2)
+        _ == _ = False
+
+hayDosDistintosConsecutivos :: [Libro] -> Bool
+hayDosDistintosConsecutivos [x] = False
+hayDosDistintosConsecutivos (x:y:xs) = x /= y || hayDosDistintosConsecutivos (y:xs) 
+
+--ghci> hayDosDistintosConsecutivos [li1,li2]
+--True
+
+--o---------o---------o--
+type Edad = Int
+-- type Nombre = String ¡Utilizo el type del ejercicio anterior!
+type Calificaciones = [Int]
+
+
+data EstadoEstudiantes = NoHayEstudiantes | EvolucionDeEstudiante Nombre Edad Calificaciones EstadoEstudiantes
+
+avanzarDeAño :: EstadoEstudiantes -> Nombre -> Bool
+avanzarDeA NoHayEstudiantes nm_tc = False
+avanzarDeAño (EvolucionDeEstudiante nm ed cal estEd) nm_tc | (nm == nm_tc) && (((fromIntegral (sum cal)) / (fromIntegral (length cal))) > 7) = True
+                                                           | (nm == nm_tc) && (((fromIntegral (sum cal)) / (fromIntegral (length cal))) <= 7) = False
+                                                           | otherwise = avanzarDeAño estEd nm_tc
+est1 = EvolucionDeEstudiante "Juancho" 14 [10,10,10] NoHayEstudiantes
+est2 = EvolucionDeEstudiante "Sara" 14 [2,5,3] NoHayEstudiantes
+est3 = EvolucionDeEstudiante "Rigoberta" 14 [2,5,3] NoHayEstudiantes
+lest1 = EvolucionDeEstudiante "Juancho" 14 [10,10,10] (EvolucionDeEstudiante "Sara" 14 [2,5,3] (EvolucionDeEstudiante "Rigoberta" 14 [2,5,3] NoHayEstudiantes))
+--ghci> avanzarDeAño lest1 "Sara"
+--False
+
+promedioGeneral :: EstadoEstudiantes -> Nombre -> Maybe Float
+promedioGeneral NoHayEstudiantes nm_tc = Nothing
+promedioGeneral (EvolucionDeEstudiante nm ed cal estEd) nm_tc | nm_tc == nm = Just ((fromIntegral (sum cal)) / (fromIntegral (length cal)))
+                                                              | otherwise = promedioGeneral estEd nm_tc
+
+-- ghci> promedioGeneral lest1 "Juancho"
+-- Just 10.0
+-- ghci> promedioGeneral lest1 "Sara"   
+-- Just 3.3333333
