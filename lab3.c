c31a5506f770bf252bad28c443ec4dbe5b1eabaa
Luca <31829436+Ifs26@users.noreply.github.com>
2024-10-05 10:46:32 -0300

    Arregle muchas cosas para ejecutar el debug y compilar

---
+++ lab3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>        /* inclusiÃ³n biblioteca estandar */
+
+int main(void)            /* devuelve un int, sin argumentos */ 
+ {
+    printf("Hello World!\n");
+
+    return 0;             /* resultado de la funciÃ³n */
+ }
+
+ /*  
+ 
+ RECORDAR:
+
+ 1. Para crear un ejecutable debo ir a Terminal -> Configure Task -> Elegir el que uso 
+                                        Terminal -> Run build task -> EJECUTABLE GENERADO!
+ 
+ 2. Para ejecutar un archivo en la consola debo utilizar 
+        ./ pegado al nombre del archivo ejecutable (exe)
+
+ 3. No me anda el debug asjdasd, SOLUCIONADO borrando una extension innecesaria!
+ 
+  */
